941e4aa6e0bac0ead018c521544ec1d18ab78f14
hunterchan <643948951@qq.com>
2025-10-31 17:50:09 +0800

    2025.10.31 17:50

--- private_set_intersection/cpp/main.cpp
+++ private_set_intersection/cpp/main.cpp
@@ -102,7 +102,7 @@ int process(){
   auto client = PsiClient::CreateWithNewKey(reveal_intersection).value();
   std::cout<<"客户端初始化密钥\n";
   
-// for test
+
   // int num_server_inputs = 10000;
   // std::vector<std::string> server_inputs(num_server_inputs);
   // for (int i = 0; i < num_server_inputs; i++) {
@@ -114,10 +114,10 @@ int process(){
   //   client_inputs[i] = absl::StrCat("Element", i*2);
   // }
   std::vector<std::string> server_inputs = {
-    "ming","Bob","hong","lan","lv","Alice","siri"
+    "Smith","Johnson","Williams","Brown","Jones","Garcia","Miller","Wilson"
   };
   std::vector<std::string> client_inputs = {
-    "Alice", "huang","Bob","siri","sir"
+    "Smith", "Johnson","Wilson","Brown","Garcia","ele"
   };
   int num_server_inputs = server_inputs.size();
   int num_client_inputs = client_inputs.size();
@@ -150,9 +150,76 @@ int process(){
   std::cout<<"交集:";
   std::cout<<sz<<std::endl;
   for(auto x:intersection){
-    std::cout<<x<<",";
+    std::cout<<client_inputs[x]<<",";
   }
   std::cout<<"\n";
+  return 0;
+}
+
+int mprocess(){
+  using namespace private_set_intersection;
+  
+  double fpr = 1. / (10000);
+  std::cout<<"fpr="<<fpr<<"\n";
+  
+  bool reveal_intersection = true;
+  std::cout<<"展示元素:"<<(reveal_intersection?"是":"否")<<"\n";
+  
+  auto server = PsiServer::CreateWithNewKey(reveal_intersection).value();
+  std::cout<<"服务端初始化密钥\n";
+  auto client = PsiClient::CreateWithNewKey(reveal_intersection).value();
+  std::cout<<"客户端初始化密钥\n";
+  
+  int num_server_inputs = 1000000;
+  std::vector<std::string> server_inputs(num_server_inputs);
+  for (int i = 0; i < num_server_inputs; i++) {
+    server_inputs[i] = absl::StrCat("Element", i);
+  }
+  int num_client_inputs = 1000000;
+  std::vector<std::string> client_inputs(num_client_inputs);
+  for (int i=0;i<num_client_inputs;i++){
+    client_inputs[i] = absl::StrCat("Element", i*2);
+  }
+  // std::vector<std::string> server_inputs = {
+  //   
+  // };
+  // std::vector<std::string> client_inputs = {
+  //   
+  // };
+  // int num_server_inputs = server_inputs.size();
+  // int num_client_inputs = client_inputs.size();
+  // std::cout<<"服务端元素:"<<server_inputs.size()<<std::endl;
+  // for(auto s:server_inputs){
+  //   std::cout<<s<<",";
+  // }
+  // std::cout<<"\n";
+  // std::cout<<"客户端元素:"<<client_inputs.size()<<std::endl;
+  // for(auto s:client_inputs){
+  //   std::cout<<s<<",";
+  // }
+  // std::cout<<"\n";
+
+
+  DataStructure ds = DataStructure::Raw;
+  psi_proto::ServerSetup setup =
+      server->CreateSetupMessage(fpr, num_server_inputs, server_inputs, ds).value();
+  std::cout<<"服务端初始化\n";
+  psi_proto::Request request = client->CreateRequest(client_inputs).value();
+  std::cout<<"客户端发送含inputs的请求request\n";
+  psi_proto::Response response = server->ProcessRequest(request).value();
+  std::cout<<"服务端处理客户端的请求request并回反馈处理结果response\n";
+  auto intersection = client->GetIntersection(setup, response).value();
+  std::cout<<"客户端接收服务端给出的response\n";
+  // for(int i=0;i<internal.size();i++){
+  //   std::cout<<intersection[i]<<"\n";
+  // }
+  auto sz = static_cast<int64_t>(intersection.size());
+  std::cout<<"交集:";
+  std::cout<<sz<<std::endl;
+  // for(auto x:intersection){
+  //   std::cout<<client_inputs[x]<<",";
+  // }
+  // std::cout<<"\n";
   return 0;
 }
 
